152b119a4d3593eb42e5e70e460e649ec46fd6e9
DipPal513 <dip.pal.513@gmail.com>
2021-09-13 15:47:04 +0600

    check

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// load data from API
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
@@ -8,7 +9,7 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
-  console.log(products)
+
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.image;
@@ -16,22 +17,27 @@ const showProducts = (products) => {
     console.log(rating)
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `
+    <div class="single-product">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4 class="">${product.title}</h4>
       <p>Category: ${product.category}</p>
-      <p>Rate: ${rating.rate}</p>
+      <p>Avg Rating: ${rating.rate}</p>
       <p>Total Rating: ${rating.count}</p>
-      <h2>Price: $ ${product.price}</h2>
+      <h3>Price: $ ${product.price}</h3>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-info">Details</button></div>
       `;
+
+      
     document.getElementById("all-products").appendChild(div);
   }
 };
+// for counting cart item
 let count = 0;
+
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
@@ -39,10 +45,10 @@ const addToCart = (id, price) => {
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
-
+// function for getting input value
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseFloat(element);
+  const converted = parseInt(element);
   return converted;
 };
 
